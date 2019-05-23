21d35f03c12d6caedf167b531bf85913b60de46a
Aditya Rawat <aditya1rawat@gmail.com>
2019-05-22 23:09:03 -0700

    Fixed Bios, Tested Side Icons(Failed), Created Typing Effect On Home Page. Fixed some bugs in the javascript and css.

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,47 @@ function toggleMenu(){
         showMenu = false;
     }
 }
+
+//Typing Effect Code Below
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Web Developer", "Programmer", "Swimmer", "Tubist"]; //Words that are typed onto animation
+const typingSpeed = 150;
+const erasingSpeed = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0; // textArray index
+let charIndex = 0; // character index
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingSpeed);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingSpeed);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingSpeed + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  setTimeout(type, newTextDelay + 250);
+});
